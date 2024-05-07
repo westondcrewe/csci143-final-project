a616711f736d7cbedbfc4262780033e8e5f2e1c7
westondcrewe <westondcrewe@gmail.com>
2024-05-06 18:31:10 -0700

    change test file to upload data, fix manage.py import error, add front-end code

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,6 +1,6 @@
 from flask.cli import FlaskGroup
 
-from project import app, db, User
+from project import app
 
 
 cli = FlaskGroup(app)
@@ -8,15 +8,12 @@ cli = FlaskGroup(app)
 
 @cli.command("create_db")
 def create_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
+    pass
 
 
 @cli.command("seed_db")
 def seed_db():
-    db.session.add(User(email="michael@mherman.org"))
-    db.session.commit()
+    pass
 
 
 if __name__ == "__main__":
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -9,31 +9,87 @@ from flask import (
     make_response
 )
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config.from_object("project.config.Config")
-db = SQLAlchemy(app)
+engine = sqlalchemy.create_engine(os.getenv('DATABASE_URL'), connect_args={'application_name': '__init__py'})
+connection = engine.connect()
+
+def print_debug_info():
+    # GET method
+    print('request.args.get("username")=', request.args.get("username"))
+    print('request.args.get("password")=', request.args.get("password"))
+
+    # POST method
+    print('request.form.get("username")=', request.form.get("username"))
+    print('request.form.get("password")=', request.form.get("password"))
+
+    # cookies
+    print('request.cookies.get("username")=', request.cookies.get("username"))
+    print('request.cookies.get("password")=', request.cookies.get("password"))
 
 
-class User(db.Model):
-    __tablename__ = "users"
+def are_credentials_good(username, password):
+    sql = sqlalchemy.sql.text("""
+            SELECT id_user 
+            FROM users 
+            WHERE username = :username 
+              AND password = :password;
+    """)
+    res = connection.execute(sql, {
+        'username': username,
+        'password': password})
+    if res.fetchone() is None:
+        return False
+    else:
+        return True
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
 
-    def __init__(self, email):
-        self.email = email
+def get_tweets(page_num = 1, tweets_per_page=25):
+    tweets = []
+    page_offset = (page_num - 1) * tweets_per_page
+    sql_tweet = sqlalchemy.sql.text("""
+        SELECT id_user, text, created_at, id_tweet
+        FROM tweets
+        ORDER BY created_at DESC 
+        LIMIT :limit OFFSET :offset
+    """)
+    res_tweet = connection.execute(sql_tweet, {
+        'limit': tweets_per_page,
+        'offset': page_offset })
+    for tweet in res_tweet.fetchall():
+        # get username by id_user of this tweet
+        sql_user = sqlalchemy.sql.text("""
+            SELECT id_user, username
+            FROM users
+            WHERE id_user=:id_user
+        """)
+        res_user = connection.execute(sql_user, {
+            'id_user': tweet[0] })
+        user = res_user.fetchone()
+        username = user[1]
+        # get text and created_at of this tweet
+        text = tweet[1]
+        created_at = tweet[2]
+        tweets.append({
+            'username': username,
+            'text': tweet[1],
+            'created_at': created_at})
+    return tweets
 
 
 @app.route("/")
 def root():
-    return render_template('root.html')
-
-@app.route("/login")
-def login():
-    return render_template('login.html')
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    credentials = are_credentials_good(username, password)
+    tweets = get_tweets()
+    page_num = int(request.args.get('page', 1))
+    tweets = get_tweets(page_num)#, 20)
+    next_page = page_num+1
+    prev_page = 1 if page_num==0 else page_num-1
+    return render_template('root.html', tweets=tweets, logged_in=credentials, username=username, page_num=page_num, next_page=next_page, prev_page=prev_page)
 
 @app.route("/logout")
 def logout():
@@ -51,27 +107,3 @@ def create_messages():
 def search():
     return "search\n"
 
-'''
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
-
-
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
-
-'''
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
